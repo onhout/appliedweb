ca5a378159cbb0074a4c465dba91a497638c4744
Peter Liu <onhout@gmail.com>
2018-08-22 21:22:00 -0700

    changed to react instead

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,12 @@
-const {environment} = require('@rails/webpacker')
-const vue = require('./loaders/vue')
+const {environment} = require('@rails/webpacker');
+const vue = require('./loaders/vue');
 
-environment.loaders.append('vue', vue)
-module.exports = environment
+environment.loaders.append('vue', vue);
+environment.loaders.get('sass').use.splice(-1, 0, {
+    loader: 'resolve-url-loader',
+    options: {
+        attempts: 1
+    }
+});
+
+module.exports = environment;
