57bfbd0f7158a44c0940834034a56fe0f8cffcd6
Peter Liu <onhout@gmail.com>
2018-08-23 21:49:59 -0700

    updated for react-bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,6 @@
 const {environment} = require('@rails/webpacker');
-const vue = require('./loaders/vue');
 
-environment.loaders.append('vue', vue);
+
 environment.loaders.get('sass').use.splice(-1, 0, {
     loader: 'resolve-url-loader',
     options: {
