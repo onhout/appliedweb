50be5974e0513e6b60bddc3832ba748bf098c155
Peter Liu <onhout@gmail.com>
2018-08-26 21:42:14 -0700

    unfortunately, alstar is not bootstrap4, so fk it?

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,17 @@
 const {environment} = require('@rails/webpacker');
+const webpack = require('webpack');
 
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+        $: 'jquery',
+        JQuery: 'jquery',
+        jquery: 'jquery',
+        'window.Tether': "tether",
+        Popper: ['popper.js', 'default'], // for Bootstrap 4
+    })
+)
 
 environment.loaders.get('sass').use.splice(-1, 0, {
-    loader: 'resolve-url-loader',
-    options: {
-        attempts: 1
-    }
+    loader: 'resolve-url-loader'
 });
 
 module.exports = environment;
